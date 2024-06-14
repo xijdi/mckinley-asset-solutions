70d4fb9878a375f49f00a1a87b3346f0faf63b35
Your Name <mckinley.aaric@gmail.com>
2024-06-14 09:06:55 -0700

    General update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './static/css/main.562dba8d.css';
+import './styles.css';
 
 function App() {
     return (
