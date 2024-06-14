c058ef525614cf5c580fc2aedda8c46b4fd1fce0
Your Name <mckinley.aaric@gmail.com>
2024-06-14 08:45:23 -0700

    Resolving conflicts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './static/css/main.562dba8d.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+    return (
+        <div className="container text-center">
+            <h1>Welcome to McKinley Asset Solutions</h1>
+        </div>
+    );
+}
 
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
